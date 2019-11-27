2355ce18ec2e7721e5b65e23b774cf0024d9fcbe
ehebrard <emmanuel.hebrard@gmail.com>
2019-11-27 13:49:29 +0100

    bug fixed

--- code/src/include/DataSet.hpp
+++ code/src/include/DataSet.hpp
@@ -4,30 +4,27 @@
 
 #include <boost/dynamic_bitset.hpp>
 
+#include "CmdLine.hpp"
 #include "SparseSet.hpp"
 
 #ifndef _PRIMER_DATASET_HPP
 #define _PRIMER_DATASET_HPP
 
 using namespace boost;
+using namespace std;
 
 typedef dynamic_bitset<> instance;
 
 namespace primer {
+
 /**********************************************
 * DataSet
 **********************************************/
 /// Representation of a list of examples
 class DataSet {
 
-  // private:
-  //   dynamic_bitset<> positive_mask;
-  //   dynamic_bitset<> negative_mask;
-
 private:
-  string pretty(const int f) const {
-    return (f < numFeature() ? "" : "¬") + to_string(f % numFeature());
-  }
+  string pretty(const int f) const;
 
 public:
   /*!@name Parameters*/
@@ -46,229 +43,121 @@ public:
 public:
   /*!@name Constructors*/
   //@{
-  explicit DataSet() {}
-  explicit DataSet(const int n_feature) {
-    for (auto i{1}; i <= n_feature; ++i) {
-      string f("f_" + std::to_string(i));
-      addFeature(f);
-    }
-  }
+  explicit DataSet();
+  explicit DataSet(const int n_feature);
 
-  void reserve(const size_t n) {
-    X.reserve(n);
-    example[1].reserve(n);
-    example[0].reserve(n);
-  }
+  void reserve(const size_t n);
   //@}
 
   /*!@name Accessors*/
   //@{
-  instance &operator[](const size_t idx) { return X[idx]; }
+  instance &operator[](const size_t idx);
 
-  template <typename RandomIt> void setFeatures(RandomIt beg, RandomIt end) {
-    auto n_feature = (end - beg);
-    feature_label.reserve(n_feature);
-    for (auto f{beg}; f != end; ++f)
-      addFeature(*f);
-  }
-  void addFeature(string &f) { feature_label.push_back(f); }
+  template <typename RandomIt> void setFeatures(RandomIt beg, RandomIt end);
+  void addFeature(string &f);
+  void add(instance x_, const bool y);
+  void push(instance x_, const bool y);
 
-  void add(instance x_, const bool y) {
+  template <typename RandomIt> void add(RandomIt beg, RandomIt end);
 
-    while (x_.size() > numFeature()) {
-      string s("f" + to_string(numFeature() + 1));
-      addFeature(s);
-    }
-		
-    instance x = x_;
-    instance xp = x_;
+  size_t numFeature() const;
+  size_t size() const;
+  size_t count() const;
+  size_t volume() const;
 
-    cout << x << endl;
+  instance NOT(instance &e) const;
 
-    x.resize(2 * numFeature(), true);
-
-    cout << x << endl;
-
-    xp.resize(2 * numFeature(), false);
-    x <<= numFeature();
-
-    cout << x << endl;
-
-    x.flip();
-
-    cout << x << endl;
-
-    x |= xp;
-
-    cout << x_ << " -> " << x << endl;
-
-    exit(1);
-
-    example[y].safe_add(X.size());
-    X.push_back(x);
-  }
-
-  template <typename RandomIt> void add(RandomIt beg, RandomIt end) {
-    instance x;
-    x.resize(numFeature(), false);
-    x.resize(2 * numFeature(), true);
-    for (auto s{beg}; s != end - 1; ++s) {
-      std::stringstream convert(*s);
-
-      bool v{false};
-      convert >> v;
-
-      if (v) {
-        x.set(s - beg);
-        x.reset(s - beg + numFeature());
-      }
-    }
-
-    std::stringstream convert(*(end - 1));
-
-    bool y{false};
-    convert >> y;
-
-    example[y].safe_add(X.size());
-    X.push_back(x);
-    // add(x, y);
-  }
-  // void add(instance &x, const bool y) {
-  //   example[y].safe_add(X.size());
-  //   X.push_back(x);
-  // }
-
-  size_t numFeature() const { return feature_label.size(); }
-  size_t size() const { return X.size(); }
-  size_t count() const { return example[0].count() + example[1].count(); }
-  size_t volume() const {
-    auto v{0};
-    for (auto c{0}; c < 2; ++c)
-      for (auto e : example[c])
-        v += X[e].count();
-    return v;
-  }
-
-  instance NOT(instance &e) const {
-    instance not_e = e;
-    not_e >>= numFeature();
-    not_e |= (e << numFeature());
-    return not_e;
-  }
-
-  double conditional_entropy(const int feature) {
-
-    auto not_feature = (feature + numFeature());
-
-    // cout << pretty(feature) << " / " << pretty(not_feature) << endl;
-
-    double npos[2] = {0, 0};
-    double nneg[2] = {0, 0};
-    double *count[2] = {nneg, npos};
-    for (auto c{0}; c < 2; ++c)
-      for (auto e : example[c])
-        if (X[e][feature] != X[e][not_feature])
-          ++count[c][X[e][feature]];
-
-    // for (auto v{0}; v < 2; ++v)
-    // 	for (auto c{0}; c < 2; ++c)
-    // 		cout << feature << "=" << v << ": " << count[c][v] << (c ? " positive"
-    // : " negative") << " examples\n";
-
-    double entropy{0};
-    double total_size{
-        static_cast<double>(example[0].count() + example[1].size())};
-
-    for (auto val{0}; val < 2; ++val) {
-      //
-      // cout << "compute entropy of dataset w.r.t. " << pretty((val ? feature :
-      // not_feature)) << " (" << count[0][val] << "/" << count[1][val] <<
-      // "):\n";
-
-      auto val_size{count[0][val] + count[1][val]};
-      // auto ratio{val_size / total_size};
-      double entropy_val{0};
-      for (auto c{0}; c < 2; ++c) {
-        if (count[c][val] != 0 and count[c][val] != val_size) {
-          entropy_val -=
-              (count[c][val] / val_size) * std::log(count[c][val] / val_size);
-          // cout << " + " << -(count[c][val] / val_size) *
-          // std::log(count[c][val] / val_size);
-        }
-        // else cout << " + 0";
-      }
-
-      // cout << " = " << (entropy_val * val_size / total_size) << endl;
-
-      entropy += (entropy_val * val_size / total_size);
-                }
-
-                // cout << " ==> " << -entropy << endl;
-
-                return entropy;
-  }
+  double conditional_entropy(const int feature);
   //@}
 
   /*!@name List Manipulation*/
   //@{
   // keeps only n positive examples, chosen randomly with a uniform distribution
   template <class random>
-  void uniform_sample(const int c, const size_t n, random generator) {
-    while (example[c].count() > n) {
-      example[c].remove_back(example[c][generator() % example[c].count()]);
-    }
-  }
+  void uniform_sample(const int c, const size_t n, random generator);
   //@}
 
   /*!@name Business*/
   //@{
   // look for examples that are both positive and negative and remove them
-  void filter() {
-		vector<int> to_remove;
-    for (auto neg : example[0])
-      for (auto pos : example[1])
-        if (X[neg] == X[pos]) {
-					to_remove.push_back(neg);
-					to_remove.push_back(pos);
-        }
-		for(auto e{to_remove.begin()}; e!=to_remove.end(); e+=2) {
-			for(int i{0}; i<2; ++i)
-				example[i].remove_back(*(e+i));
-		}
-  }
+  void filter();
 
   // the set of true features of x1 that are false in x2
   // we cannot do set difference because features might neighter be true nor
   // false
-  void getContradictingFeatures(instance &x1, instance &x2, instance &buffer) {
-    buffer = NOT(x2);
-    // buffer = x2;
-    // buffer >>= nFeature() / 2;
-    // buffer |= (x2 << numFeature() / 2);
-
-    buffer &= x1;
-  }
+  void getContradictingFeatures(instance &x1, instance &x2, instance &buffer);
 
   void addExplanation(instance &impl, const bool y, const int limit,
-                      vector<int> &entailed) {
-    entailed.clear();
-    // remove all explained examples
-    for (auto e : example[y]) {
-      if (e > limit)
-        break;
+                      vector<int> &entailed);
 
-      if (impl.is_subset_of(X[e]))
-        entailed.push_back(e);
+  template <typename R>
+  void computeDecisionSet(Options &opt, R &random_generator);
+
+  void close();
+
+  bool classify(instance &x);
+  //@}
+
+  /*!@name Miscellaneous*/
+  //@{
+  std::ostream &to_csv(std::ostream &os) const;
+
+  std::ostream &to_txt(std::ostream &os) const;
+
+  std::ostream &display(std::ostream &os) const;
+
+  std::ostream &display_example(std::ostream &os, const instance e) const;
+  //@}
+
+  /*!@name Verification*/
+  //@{
+  void verify();
+  //@}
+};
+
+template <typename RandomIt>
+void DataSet::setFeatures(RandomIt beg, RandomIt end) {
+  auto n_feature = (end - beg);
+  feature_label.reserve(n_feature);
+  for (auto f{beg}; f != end; ++f)
+    addFeature(*f);
+}
+
+template <typename RandomIt> void DataSet::add(RandomIt beg, RandomIt end) {
+  instance x;
+  x.resize(numFeature(), false);
+  x.resize(2 * numFeature(), true);
+  for (auto s{beg}; s != end - 1; ++s) {
+    std::stringstream convert(*s);
+
+    bool v{false};
+    convert >> v;
+
+    if (v) {
+      x.set(s - beg);
+      x.reset(s - beg + numFeature());
     }
-    for (auto e : entailed)
-      example[y].remove_front(e);
-
-    // add the explanation instead
-    add(impl, y);
   }
 
-	template< typename R >
-  void computeDecisionSet(Options& opt, R& random_generator) {
+  std::stringstream convert(*(end - 1));
+
+  bool y{false};
+  convert >> y;
+
+  example[y].safe_add(X.size());
+  X.push_back(x);
+  // add(x, y);
+  }
+
+  template <class random>
+  void DataSet::uniform_sample(const int c, const size_t n, random generator) {
+    while (example[c].count() > n) {
+      example[c].remove_back(example[c][generator() % example[c].count()]);
+    }
+  }
+
+  template <typename R>
+  void DataSet::computeDecisionSet(Options &opt, R &random_generator) {
     auto c{0};
 
     // verify();
@@ -345,7 +234,7 @@ public:
 
         // there is already a feature of the explanation that contradicts X[j],
         // so no need to take X[j] into account
-				getContradictingFeatures(X[j], implicant, contradicting_features);
+        getContradictingFeatures(X[j], implicant, contradicting_features);
         if (!contradicting_features.none()) {
           if (opt.verbosity >= Options::SOLVERINFO) {
             cout << "skip " << j << " = ";
@@ -360,7 +249,7 @@ public:
         getContradictingFeatures(X[i], X[j], contradicting_features);
 
         if (opt.verbosity >= Options::SOLVERINFO) {
-					cout << j << ": ";
+          cout << j << ": ";
           display_example(cout, X[i]);
           cout << " \\ " << setw(4) << j << ":";
           display_example(cout, X[j]);
@@ -404,7 +293,9 @@ public:
       if (opt.verbosity >= Options::YACKING) {
         cout << " -> add " << X.size() << ": ";
         display_example(cout, implicant);
-        cout << " (" << implicant.count() << ")" << endl << endl;
+        cout << " (" << implicant.count() << "/" << implicant.size() << ")"
+             << endl
+             << endl;
       }
 
       // add the explanation to example[c] and remove all the examples that are
@@ -421,199 +312,9 @@ public:
           cout << endl;
         }
     }
-  }
-
-  void close() {
-    literal_count.reserve(size());
-    for (auto c{0}; c < 2; ++c)
-      for (auto e : example[c])
-        literal_count[e] = X[e].count();
-  }
-
-  bool classify(instance &x) {
-    for (auto c{0}; c < 2; ++c)
-      for (auto e : example[c])
-        if (X[e].is_subset_of(x))
-          return c;
-
-    instance buffer;
-    double proba[2];
-
-    // #contradictions / #number literal = probability that x is not of class c
-
-    for (auto c{0}; c < 2; ++c)
-      for (auto e : example[c]) {
-        buffer = X[e];
-        buffer -= x;
-        auto nl{buffer.count()};
-        proba[c] *=
-            (static_cast<double>(nl) / static_cast<double>(literal_count[e]));
-      }
-
-    return proba[1] > proba[0];
-  }
-  //@}
-
-  /*!@name Miscellaneous*/
-  //@{
-  std::ostream &to_csv(std::ostream &os) const {
-    for (auto i{0}; i < numFeature(); ++i)
-      os << feature_label[i] << ",";
-    os << "label" << endl;
-    for (auto c{0}; c < 2; ++c)
-      for (auto j : example[c]) {
-        for (auto i{0}; i < numFeature(); ++i)
-          os << X[j][i] << ",";
-        os << c << endl;
-      }
-
-    return os;
-  }
-
-  std::ostream &to_txt(std::ostream &os) const {
-    //     for (auto i{0}; i < numFeature(); ++i)
-    //       os << feature_label[i] << " " ;
-    // os << "label" << endl;
-    for (auto c{0}; c < 2; ++c)
-      for (auto j : example[c]) {
-        for (auto i{0}; i < numFeature(); ++i)
-          os << X[j][i] << " ";
-        os << c << endl;
-      }
-
-    return os;
-  }
-
-  std::ostream &display(std::ostream &os) const {
-
-    os << "features:";
-    // for (auto l : feature_label)
-    for (auto i{0}; i < numFeature(); ++i)
-      os << " " << feature_label[i];
-    os << endl;
-
-    // for (auto i : example[1])
-    //   os << X[i] << ": +" << std::endl;
-    // for (auto i : example[0])
-    //   os << X[i] << ": -" << std::endl;
-
-    os << "POSITIVE EXAMPLES:\n";
-    for (auto i : example[1]) {
-      cout << i << ": "; //<< X[i] << " ";
-      display_example(os, X[i]);
-      os << endl;
-    }
-    os << "NEGATIVE EXAMPLES:\n";
-    for (auto i : example[0]) {
-      cout << i << ": "; //<< X[i] << " ";
-      display_example(os, X[i]);
-      os << endl;
-    }
-    return os;
-  }
-
-  std::ostream &display_example(std::ostream &os, const instance e) const {
-
-    auto flag{false};
-    auto nxt{false};
-    int last;
-    size_t n{e.count()};
-
-    os << "(";
-    for (auto f{0}; f < 2 * numFeature(); ++f) {
-      if (e[f]) {
-
-        // os << "[" << f << "]";
-
-        if (!--n) {
-          if (flag)
-            os << ",";
-          os << pretty(f) << ")"; // feature_label[f];
-          break;
-        }
-
-        if (f < 2 * numFeature() - 1 and e[f + 1])
-          nxt = true;
-        else
-          nxt = false;
-
-        if (!nxt or f != last + 1 or f == numFeature()) {
-          if (flag)
-            os << ",";
-          os << pretty(f); // feature_label[f];
-          flag = true;
-        } else if (flag) {
-          os << "..";
-          flag = false;
-        }
-
-        last = f;
-      }
-    }
-
-    return os;
-  }
-  //@}
-
-  /*!@name Verification*/
-  //@{
-  void verify() {
-    // check that every example is entailed by an explanation of its class and
-    // that no explanation entails an example of the other class
-
-    instance not_covered[2];
-    for (auto c{0}; c < 2; ++c) {
-      not_covered[c].resize(X.size(), false);
-      for (auto i{-example[c].start()}; i < 0; ++i)
-        not_covered[c].set(example[c][i]);
-    }
-
-    instance contradictions, not_expl;
-    for (auto c{0}; c < 2; ++c)
-      for (auto explanation : example[c]) {
-        not_expl = NOT(X[explanation]);
-
-        for (auto i{-example[c].start()}; i < 0; ++i)
-          if (X[explanation].is_subset_of(X[example[c][i]]))
-            not_covered[c].reset(example[c][i]);
-
-        for (auto i{-example[1 - c].start()}; i < 0; ++i)
-          if (X[explanation].is_subset_of(X[example[c][i]])) {
-            cerr << "rule " << explanation << ": " << X[explanation] << " = ";
-            display_example(cerr, X[explanation]);
-            cerr << " for class " << c << " entails example " << example[c][i]
-                 << ": " << X[example[c][i]] << " = ";
-            display_example(cerr, X[example[c][i]]);
-            cerr << " of class " << (1 - c) << "!!\n";
-            exit(1);
-          }
-
-        for (auto counter_example : example[1 - c]) {
-          contradictions = (not_expl & X[counter_example]);
-          if (contradictions.none()) {
-            cerr << explanation << ": ";
-            display_example(cerr, X[explanation]);
-            cerr << " does not contradict " << counter_example << ": ";
-            display_example(cerr, X[counter_example]);
-            cerr << endl;
-            exit(1);
-          }
-        }
-      }
-
-    for (auto c{0}; c < 2; ++c) {
-      if (!not_covered[c].none()) {
-        cerr << "examples " << not_covered[c] << " are covered by no rule!!\n";
-        exit(1);
-      }
-    }
-  }
-};
-
-std::ostream &operator<<(std::ostream &os, const DataSet &x) {
-  return x.display(os);
 }
 
+std::ostream &operator<<(std::ostream &os, const DataSet &x);
 }
 
 #endif // _PRIMER_DATASET_HPP
