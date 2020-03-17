df1c203a9a954efac89a0301e0e7a9d66ea36040
ehebrard <emmanuel.hebrard@gmail.com>
2020-03-17 09:27:22 +0100

    files

---
+++ code/src/include/Tree.hpp
@@ -0,0 +1,57 @@
+
+#include <iostream>
+#include <vector>
+
+#include "DataSet.hpp"
+
+#ifndef __TREE_HPP
+#define __TREE_HPP
+
+/**********************************************
+* ListTree
+**********************************************/
+
+using namespace std;
+
+namespace primer {
+
+class Tree
+{
+
+private:
+  ///
+  vector<int> left_child;
+	vector<int> feature;
+  //@}
+
+public:
+  /*!@name Constructors*/
+  //@{
+	explicit Tree();
+	
+	int getFeature(const int node) const;
+	
+	bool predict(const instance& x) const;
+	
+	int getLeaf(const instance& x) const;
+
+	int predict(const DataSet& data) const;
+	
+	int child(const int x, const bool t) const;
+	
+	void addNode(const int p, const int f);
+	//@}
+
+  /*!@name Miscellaneous*/
+  //@{
+  std::ostream &display(std::ostream &os) const;
+  //@}
+};
+
+std::ostream& operator<<(std::ostream& os, const Tree& x);
+
+
+}
+
+
+#endif // __TREE_HPP
