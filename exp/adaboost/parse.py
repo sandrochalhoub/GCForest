3e11c54ef00a4fe17f46f4e91713ad9e06d10a74
Louis Jean <ljean@etud.insa-toulouse.fr>
2020-06-09 13:08:41 +0200

    Adaboost parse

--- exp/adaboost/parse.py
+++ exp/adaboost/parse.py
@@ -4,7 +4,7 @@ from rocknrun import *
 
 
 # Parsers should return a dict stat-name -> value-list. value-list is a list of the different values that the stat takes during a run
-class AdaboostParser(object):
+class GenericParser(object):
 
     def __call__(self,respath):
         res = {}
@@ -19,14 +19,26 @@ class AdaboostParser(object):
 
 if __name__ == '__main__':
     e = Experiment()
-    parsers = dict([(m, AdaboostParser()) for m in e.all_methods])
+    parsers = dict([(m, GenericParser()) for m in e.all_methods])
 
     # Data needed for the table:
     # - number of examples in dataset
     # - train accuracy & test accuracy
 
+    # What I want to do:
+    # Print a table with header:
+    header = """
+        \\begin{tabular}{|*{6}{c|}}
+        \\hline
+        \\multirow{2}{*}{Dataset} & \\multirow{2}{*}{\\#s} & \\multicolumn{2}{l|}{BFS acc} & \\multicolumn{2}{l|}{CART acc} \\\\
+        \\cline{3-6}
+          &  & train & test & train & test \\\\
+        \\hline
+        """
+
+
     o = Observation(e, parsers)
-    o.write_summary_table('tex/summary.tex', ['number of conflicts', 'cpu time'], precisions=[0,2])
-    o.write_large_table('tex/all.tex', ['number of conflicts', 'cpu time'], precisions=[0,2])
-    o.write_cactus('tex/cactus.tex', X='number of conflicts', Y='cpu time', precision=1, epsilon=.01)
+    o.write_summary_table('tex/summary.tex', ['train', 'test'], precisions=[0,2])
+    o.write_large_table('tex/all.tex', ['train', 'test'], precisions=[0,2])
+    # o.write_cactus('tex/cactus.tex', X='number of conflicts', Y='cpu time', precision=1, epsilon=.01)
     compile_latex()
