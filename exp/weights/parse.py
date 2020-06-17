d33f8e982ba9dbc024eb7878f2c9673c44f96afc
Louis Jean <lsj456@gmail.com>
2020-06-17 14:47:04 +0200

    Execute weights/parse.py to fix errors

--- exp/weights/parse.py
+++ exp/weights/parse.py
@@ -46,13 +46,12 @@ class DTParser(object):
                     self.store('proof'+stat,val,res)
                 self.store(stat,val,res)
 
-        print str(res)
         return res
 
 
 if __name__ == '__main__':
     e = Experiment()
-    parsers = dict([(m, GenericParser()) for m in e.all_methods])
+    parsers = dict([(m, DTParser()) for m in e.all_methods])
     benches = [Benchmark([b]) for b in e.all_benchmarks]
 
     # Parsed material: #s, %duplicate, time method 1, time method 2
@@ -62,22 +61,27 @@ if __name__ == '__main__':
     time = Statistic('time', label= 'time', precision=lambda x:3)
     sample_count = Statistic('sample_count', label='\\#s')
     dratio = Statistic('dratio', label='\\% dupli.', precision=lambda x:1)
-    error = Statistic('accuracy', label='acc.', precision=lambda x:3)
+    accuracy = Statistic('accuracy', label='acc.', precision=lambda x:3)
 
     m_no_weights_success = Method('no weights', stats=[time])
     m_weights_success = Method('weights', stats=[time, dratio])
 
-    m_no_weights_fails = Method('no weights', stats=[error])
-    m_weights_fails = Method('weights', stats=[error, dratio])
+    m_no_weights_fails = Method('no weights', stats=[accuracy])
+    m_weights_fails = Method('weights', stats=[accuracy, dratio])
 
     benches_success = []
     benches_fail = []
 
     for b in benches:
-        if len(o.data[m_no_weights_success][b][e.seeds[0]]["optimal"]) > 0:
+        m = next(iter( e.all_methods))
+        
+        if o.data[m][b.label][e.seeds["weighted"][0]]["optimal"][-1] != 0:
             benches_success.append(b)
         else:
             benches_fail.append(b)
+
+    print benches_success
+    print benches_fail
 
     # o.write_table('tex/weighted.tex', [m_no_weights,m_weights], benches, info=[sample_count])
     o.write_table('tex/weighted_success.tex', [m_no_weights_success,m_weights_success], benches_success)
