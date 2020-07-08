f5faa322ff1f75e31eebd654172e500990a59ba8
Louis Jean <lsj456@gmail.com>
2020-07-08 18:13:42 +0200

    Add missing files

---
+++ code/bud_first_search/adaboost.py
@@ -0,0 +1,22 @@
+from . import wrapper
+from .utils import *
+
+class AdaBoostClassifier:
+    def __init__(self):
+        self.opt = wrapper.parse(to_str_vec(["adaboost.py", "--file", "none"]))
+
+    def fit(self, X, Y):
+        self.algo = wrapper.Adaboost(self.opt)
+
+        for x, y in zip(X, Y):
+            self.algo.addExample(x + [y])
+
+        self.algo.train()
+
+    def predict(self, X):
+        Y = []
+
+        for x in X:
+            Y.append(self.algo.predict(x))
+
+        return Y
