10e9425f21b031f103d24dccd1330464d9b5399d
ehebrard <emmanuel.hebrard@gmail.com>
2020-12-08 07:20:15 +0100

    missing tables

--- code/setup.py
+++ code/setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bud-first-search", # Replace with your own username
+    name="blossom", # Replace with your own username
     version="0.0.1",
     author="E. Hebrard, L. Jean",
     author_email="hebrard@laas.fr, ljean@laas.fr",
-    description="Bud first search is an algorithm to learn optimal decision trees",
+    description="Blossom is an algorithm to learn optimal decision trees",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://redmine.laas.fr/laas/users/ehebrard/deeplever.git",
