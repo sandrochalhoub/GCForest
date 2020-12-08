2a9c4975e6583a806ec6c51ac8a4b7bd241c2242
ehebrard <emmanuel.hebrard@gmail.com>
2020-12-08 08:12:33 +0100

    new wrapper

--- code/blossom/wrapper/include/blossom.h
+++ code/blossom/wrapper/include/blossom.h
@@ -4,9 +4,10 @@
 #include "Tree.hpp"
 #include "Backtrack.hpp"
 #include "CmdLine.hpp"
+#include "WeightedDataset.hpp"
 #include "Adaboost.hpp"
 
 #define SWIG_FILE_WITH_INIT
 
 extern DTOptions parse(std::vector<std::string> params);
-extern void read_binary(blossom::WeightedDataset &input, DTOptions &opt);
+extern void read_binary(blossom::WeightedDataset &input, blossom::DTOptions &opt);
