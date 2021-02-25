7c0791b430c089b8dce538d542d3fbbde80d1687
ehebrard <emmanuel.hebrard@gmail.com>
2021-02-25 17:51:35 +0100

    ok

--- code/examples/src/binarizer.cpp
+++ code/examples/src/binarizer.cpp
@@ -85,13 +85,13 @@ int main(int argc, char *argv[]) {
   WeightedDataset<int> input;
 
   ////// READING
-  // try {
-  //   read_binary(input, opt);
-  // } catch (const std::exception &e) {
-  if (opt.verbosity >= DTOptions::NORMAL)
-    cout << "c format not recognized or input non-binary, binarizing...\n";
-  read_non_binary(input, opt);
-  // }
+  try {
+    read_binary(input, opt);
+  } catch (const std::exception &e) {
+		if (opt.verbosity >= DTOptions::NORMAL)
+   	 	cout << "c binarizing...\n";
+  	read_non_binary(input, opt);
+  }
 
   if (opt.sample < 1)
     input.sample(opt.sample);
